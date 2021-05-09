97cdb271e64f414b01697bb0f17acdc57637a986
Kenan <kenan.karic@outlook.com>
2021-05-09 11:44:41 +0200

    Minor Di refactor

--- src/framework/di/ApplicationModule.kt
+++ src/framework/di/ApplicationModule.kt
@@ -1,8 +1,10 @@
 package io.aethibo.framework.di
 
+import io.aethibo.repositories.utils.DatabaseFactory
 import io.aethibo.utils.JwtService
 import org.koin.dsl.module
 
 val applicationModule = module {
     single { JwtService }
+    single { DatabaseFactory }
 }
--- src/framework/di/RepositoriesModule.kt
+++ src/framework/di/RepositoriesModule.kt
@@ -1,13 +1,11 @@
 package io.aethibo.framework.di
 
-import io.aethibo.repositories.utils.DatabaseFactory
 import io.aethibo.repositories.DefaultMainRepository
 import io.aethibo.repositories.MainRepository
 import org.koin.dsl.module
 
 val repositoriesModule = module {
 
-    single { DatabaseFactory }
     // single<MainRepository> { InMemoryRepository() }
     single<MainRepository> { DefaultMainRepository() }
 }
