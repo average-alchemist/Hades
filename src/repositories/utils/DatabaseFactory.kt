f629e0e87f2692cb5c82d0893633a19e2645245d
Kenan <kenan.karic@outlook.com>
2021-05-09 11:43:33 +0200

    Docker/Hikari init setup

--- src/repositories/utils/DatabaseFactory.kt
+++ src/repositories/utils/DatabaseFactory.kt
@@ -12,7 +12,29 @@ object DatabaseFactory {
 
     init {
         // In memory / keep alive between connections/transactions
-        Database.connect("jdbc:h2:mem:regular;DB_CLOSE_DELAY=-1;", "org.h2.Driver")
+        // Database.connect("jdbc:h2:mem:regular;DB_CLOSE_DELAY=-1;", "org.h2.Driver")
+
+        // PostgreSQL
+        Database.connect(
+            url = "jdbc:postgresql://database:5432/thoughtsDb",
+            driver = "org.postgresql.Driver",
+            user = "username",
+            password = "secret"
+        )
+
+        /**
+         * Hikari setup - with connection pooling
+         */
+        /*val config = HikariConfig().apply {
+            jdbcUrl = "jdbc:postgresql://database:5432/thoughtsDb"
+            driverClassName = "org.postgresql.Driver"
+            username = "username"
+            password = "secret"
+            maximumPoolSize = 10
+        }
+
+        val dataSource = HikariDataSource(config)
+        Database.connect(dataSource)*/
 
         transaction {
             SchemaUtils.create(Thoughts)
